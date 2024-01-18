ac3efeee33788c5ebf8630eabe6bfb02cb769974
Tiago M. Neves <tiagomneves@protonmail.com>
2024-01-18 19:08:15 +0000

    Versão estável - Arch

---
+++ gruvbox_colours.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#Gruvbox Colours
+
+# Normal
+export gbd_black=#0x282828
+export gb_dred=#0xcc241d
+export gb_dgreen=0x98971a
+export gb_dyellow='0xd79921'
+export gb_dblue='0x458588'
+export gb_dmagenta='0xb16286'
+export gb_dcyan='0x689d6a'
+export gb_dwhite='0xa89984'
+
+# Bright colours
+export gbd_lblack='0x928374'
+export gbd_lred='0xfb4934'
+export gbd_lgreen='0xb8bb26'
+export gbd_lyellow='0xfabd2f'
+export gbd_lblue='0x83a598'
+export gbd_lmagenta='0xd3869b'
+export gbd_lcyan='0x8ec07c'
+export gbd_lwhite='0xebdbb2'
+
---
+++ rofi/freq_apps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "code-oss"
+echo "brave"
+echo "firefox"
+echo "Result 4"
+echo "Result 5"
+
+ls /usr/bin
+
