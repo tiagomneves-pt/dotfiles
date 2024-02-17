c01ab8a743de9feeae384bd2e4cf95bc5497542e
Tiago M. Neves <tiagomneves@protonmail.com>
2024-02-17 11:41:18 +0000

    none-ls adicionado

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return{
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {"lua", "vim", "html", "css", "php", "c", "bash"},
+      auto_install = true,
       highlight = { enable = true },
       indent = { enable = true }
     })
