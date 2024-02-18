c858a0938e505d8f2c7d61010c0656bb06f5f1e1
Tiago M. Neves <tiagomneves@protonmail.com>
2024-02-18 18:00:26 +0000

    Debugger adicionado

---
+++ nvim/lua/plugins/debugging.lua
@@ -0,0 +1,33 @@
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"leoluz/nvim-dap-go",
+	},
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
+
+    require("dap-go").setup()
+    require("dapui").setup()
+
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+		vim.keymap.set("n", "<Leader>dt", function()
+			dap.toggle_breakpoint()
+		end)
+		vim.keymap.set("n", "<Leader>dc", function()
+			dap.continue()
+		end)
+	end,
+}
